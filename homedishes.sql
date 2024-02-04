1b9513ab8102943c942c83f1ce1db93ce34c4a3c
Mateusz-Kuziak <158851132+Mateusz-Kuziak@users.noreply.github.com>
2024-02-04 20:42:42 +0100

    Add files via upload

---
+++ homedishes.sql
@@ -0,0 +1,74 @@
+ALTER TABLE `Cook` DROP FOREIGN KEY `Cook_fk0`;
+
+ALTER TABLE `CatalogItemCook` DROP FOREIGN KEY `CatalogItemCook_fk0`;
+
+ALTER TABLE `CatalogItemCook` DROP FOREIGN KEY `CatalogItemCook_fk1`;
+
+ALTER TABLE `TypeOfCatalogItem` DROP FOREIGN KEY `TypeOfCatalogItem_fk0`;
+
+ALTER TABLE `TypeOfCatalogItem` DROP FOREIGN KEY `TypeOfCatalogItem_fk1`;
+
+ALTER TABLE `Offer` DROP FOREIGN KEY `Offer_fk0`;
+
+ALTER TABLE `Offer` DROP FOREIGN KEY `Offer_fk1`;
+
+ALTER TABLE `Offer` DROP FOREIGN KEY `Offer_fk2`;
+
+ALTER TABLE `Offer` DROP FOREIGN KEY `Offer_fk3`;
+
+ALTER TABLE `CategoryOfItem` DROP FOREIGN KEY `CategoryOfItem_fk0`;
+
+ALTER TABLE `CategoryOfItem` DROP FOREIGN KEY `CategoryOfItem_fk1`;
+
+ALTER TABLE `FoodRequest` DROP FOREIGN KEY `FoodRequest_fk0`;
+
+ALTER TABLE `FoodRequest` DROP FOREIGN KEY `FoodRequest_fk1`;
+
+ALTER TABLE `Discount` DROP FOREIGN KEY `Discount_fk0`;
+
+ALTER TABLE `Discount` DROP FOREIGN KEY `Discount_fk1`;
+
+ALTER TABLE `Delivery` DROP FOREIGN KEY `Delivery_fk0`;
+
+ALTER TABLE `Delivery` DROP FOREIGN KEY `Delivery_fk1`;
+
+ALTER TABLE `Delivery` DROP FOREIGN KEY `Delivery_fk2`;
+
+ALTER TABLE `Review` DROP FOREIGN KEY `Review_fk0`;
+
+ALTER TABLE `Review` DROP FOREIGN KEY `Review_fk1`;
+
+ALTER TABLE `Review` DROP FOREIGN KEY `Review_fk2`;
+
+ALTER TABLE `RestaurantUser` DROP FOREIGN KEY `RestaurantUser_fk0`;
+
+ALTER TABLE `RestaurantUser` DROP FOREIGN KEY `RestaurantUser_fk1`;
+
+ALTER TABLE `DeliveryGuyDelivery` DROP FOREIGN KEY `DeliveryGuyDelivery_fk0`;
+
+ALTER TABLE `DeliveryGuyDelivery` DROP FOREIGN KEY `DeliveryGuyDelivery_fk1`;
+
+ALTER TABLE `LocationOfUser` DROP FOREIGN KEY `LocationOfUser_fk0`;
+
+ALTER TABLE `LocationOfUser` DROP FOREIGN KEY `LocationOfUser_fk1`;
+
+ALTER TABLE `PaymentForCook` DROP FOREIGN KEY `PaymentForCook_fk0`;
+
+ALTER TABLE `PaymentForCook` DROP FOREIGN KEY `PaymentForCook_fk1`;
+
+ALTER TABLE `BuyerPay` DROP FOREIGN KEY `BuyerPay_fk0`;
+
+ALTER TABLE `BuyerPay` DROP FOREIGN KEY `BuyerPay_fk1`;
+
+ALTER TABLE `OfferPayment` DROP FOREIGN KEY `OfferPayment_fk0`;
+
+ALTER TABLE `OfferPayment` DROP FOREIGN KEY `OfferPayment_fk1`;
+
+ALTER TABLE `UserRequest` DROP FOREIGN KEY `UserRequest_fk0`;
+
+ALTER TABLE `UserRequest` DROP FOREIGN KEY `UserRequest_fk1`;
+
+ALTER TABLE `OfferRequest` DROP FOREIGN KEY `OfferRequest_fk0`;
+
+ALTER TABLE `OfferRequest` DROP FOREIGN KEY `OfferRequest_fk1`;
+
